a92041b0190129a1c50b198a9469088f48c99a42
Dunn, Simeon <simedunn01@gmail.com>
2025-05-01 11:55:57 -0400

    pre-tailwindv4 again 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,7 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  darkMode: ["class"],
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  safelist: ["dark"],
   theme: {
     container: {
       center: true,
@@ -89,9 +85,6 @@ const config: Config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)"
       },
-      fontFamily: {
-        sans: [...fontFamily.sans]
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -113,7 +106,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [tailwindcssAnimate],
 };
 
 export default config;
